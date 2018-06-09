1d47d9d252ab161824f367482e4623d751d78429
rchipka <rchipka@vibethink.com>
2018-06-09 16:57:20 -0400

    Add context

--- TagBuilder.php
+++ TagBuilder.php
@@ -41,6 +41,8 @@ function vbtk_tag_builder($tag, $attrs = [], $children = []) {
     $attributes[] = $key . '="' . esc_attr($value) . '"';
   }
 
+  $context = new Context(array_merge([$tag], $attrs['class'] ?: []));
+
   if (sizeof($attrs) === 1) {
     if (is_numeric($attrs[0]) || is_bool($attrs[0])) {
       if (!$attrs[0]) {
@@ -128,6 +130,8 @@ function vbtk_tag_builder($tag, $attrs = [], $children = []) {
       $out .= '</' . $tag . '>';
     }
   }
+
+  $context->exit();
 
   return $out;
 }
